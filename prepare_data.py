3b239c0497e8ad38c8bb892f6a98f61bdb5f3fbe
ayla <aylataylor@gmail.com>
2022-03-25 01:11:27 -0400

    changed prepare_data.py and added outputted files

--- prepare_data.py
+++ prepare_data.py
@@ -3,14 +3,17 @@ import sys
 
 
 def main() -> None:
-    """The input file should start with a number, ie. '02_cleaned_reviews.json'"""
+    """Input file is a command line argument."""
     filename = sys.argv[1]
-    batch_number = filename[5:].split("_")[0]
+    # batch_number = filename[5:].split("_")[0]
     with open(filename, encoding='utf-8') as f:
         dicts = json.loads(f.read())
-    with open("updated_data/prepared_data_" + batch_number + ".txt", 'w', encoding='utf-8') as out_f:
-        for d in dicts:
-            out_f.write(d['review_text'] + '\n')
+    for d in dicts:
+        with open("updated_data/review_" + str(d['id']) + ".txt", 'w', encoding='utf-8') as out_f:
+            out_f.write(d['review_text'])
+    # with open("updated_data/prepared_data_" + batch_number + ".txt", 'w', encoding='utf-8') as out_f:
+    #     for d in dicts:
+    #         out_f.write(d['review_text'] + '\n')
 
 
 if __name__ == '__main__':
