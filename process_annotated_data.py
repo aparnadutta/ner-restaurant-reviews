f88b9a34ae202fc62f499dbab7ad40f10cdd765f
June Kim <72058960+haewonkim620@users.noreply.github.com>
2022-04-23 23:18:09 -0400

    Chester's changes

--- process_annotated_data.py
+++ process_annotated_data.py
@@ -1,6 +1,8 @@
 import json
+from collections import defaultdict
+
 import tqdm
-from typing import Iterable
+from typing import Iterable, Dict, Tuple, List, Optional
 
 from match_with_metadata import match_meta
 from tokenization import process_annotations
@@ -100,10 +102,99 @@ def make_conll(annotations: list[list[dict]]) -> None:
             out_f.write('\n')
 
 
+def get_label(
+        annotatorid_to_sentences: Dict[int, Optional[list[list[tuple[str, str]]]]],
+        annotator_idx: int,
+        sent_idx: int,
+        token_idx: int
+):
+    if annotatorid_to_sentences[annotator_idx] is None:
+        return None
+    else:
+        # 1 is the label in the token, label tuple
+        # print(annotatorid_to_sentences[annotator_idx][sent_idx])
+        # print(annotator_idx, sent_idx, token_idx)
+        return annotatorid_to_sentences[annotator_idx][sent_idx][token_idx][1]
+
+
+def join_annotations(
+        annotatorid_to_sentences: Dict[int, Optional[list[list[tuple[str, str]]]]]
+) -> List[List[Tuple[str, str, str, str]]]:
+    assumed_sents = None
+    ret = []
+    for annotatorid in annotatorid_to_sentences:
+        if annotatorid_to_sentences[annotatorid] is not None:
+            assumed_sents = annotatorid_to_sentences[annotatorid]
+            break
+    if not assumed_sents:
+        raise ValueError("All annotations are empty")
+    for sent_idx, sent in enumerate(assumed_sents):
+        joined_sent = []
+        try:
+            for token_idx, (token, _) in enumerate(sent):
+                label0 = get_label(annotatorid_to_sentences, 0, sent_idx, token_idx)
+                label1 = get_label(annotatorid_to_sentences, 1, sent_idx, token_idx)
+                label2 = get_label(annotatorid_to_sentences, 2, sent_idx, token_idx)
+                joined_sent.append((token, label0, label1, label2))
+            ret.append(joined_sent)
+        except IndexError:
+            print(f"Token mismatch in sentence {sent}, skipping")
+            continue
+    return ret
+
+
+def chesters_make_conll(annotations: list[list[dict]]) -> None:
+    print("Adding Metadata...")
+    reviews = dict()
+    metadata_file = read_file(METAFILE)[0]
+    metadata = [r for r in metadata_file if
+                int(r['id']) < 90]  # limit it to just the reviews we did
+    for d in metadata:
+        # make dict for looking up the reviews by id, with space to put annotation for each of us
+        reviews[d['id']] = [None, None, None]
+    for i, persons_data in enumerate(
+            annotations):  # iterate over each persons annotation data (len(annotation) = 3)
+        #  i = 0 is aparna, i = 1 is june, i = 2 is ayla
+        for x in persons_data:
+            meta = match_meta(x, metadata)
+            reviews[meta['id']][i] = meta
+    # create a file for the metadata
+    print("Creating Metadata file: \'all_annotations_with_metadata.json\'")
+    with open('all_annotations_with_metadata.json', 'w', encoding='utf-8') as f_out:
+        json.dump(reviews, f_out)
+    print("Tokenizing...")
+
+    review_list = list(reviews.items())
+    review_list.sort(key=lambda y: y[0], reverse=True)
+    # So this is a list where each item is tuple idx of article then list of None or dict of annotation
+    docid_to_sentences = defaultdict(list)
+    for docid, annotations in tqdm.tqdm(review_list):
+        if any(annotations):
+            annotatorid_to_sentences: Dict[int, Optional[list[list[tuple[str, str]]]]] = {}
+            for annotator_id, annotation in enumerate(annotations):  # iterating through each annotators annotations
+                if annotation:
+                    annotatorid_to_sentences[annotator_id] = process_annotations(annotation)
+                else:
+                    annotatorid_to_sentences[annotator_id] = None
+            joined_sents: List[List[Tuple[str, str, str, str]]] = join_annotations(annotatorid_to_sentences)
+            docid_to_sentences[docid].append(joined_sents)
+
+    with open('all_annotations.txt', 'w', encoding='utf8') as out_f:
+        for docid in docid_to_sentences:
+            print("-DOCSTART- O O O", file=out_f)
+            print(file=out_f)
+            for sentences in docid_to_sentences[docid]:
+                for sent in sentences:
+                    for token_and_labels in sent:
+                        print(" ".join([str(item) for item in token_and_labels]), file=out_f)
+                    print(file=out_f)
+
+
 def main():
     print("Reading in data from annotation files...")
     data = [read_file(PATH + x) for x in FILES]
-    make_conll(data)
+    # make_conll(data)
+    chesters_make_conll(data)
 
 
 if __name__ == '__main__':
--- tokenization.py
+++ tokenization.py
@@ -1,6 +1,6 @@
 import json
 import stanza
-from typing import NamedTuple
+from typing import NamedTuple, List, Dict, Sequence
 
 OUTSIDE = "0"
 BEGIN = "B"
@@ -27,25 +27,35 @@ def read_file(filename: str) -> dict:
     return data
 
 
-def encode_bio(tokens: list[tuple[str, int]], mentions: list[Mention]) -> list[str]:
-    """Outputs a list of the BIO tags"""
-    encoded_list = []
-    m_num = 0  # The index of the mention in the mentions list
-    for word, i in tokens:
-        if m_num < len(mentions):
-            current_mention = mentions[m_num]
-            if i == current_mention.start:
-                encoded_list.append(BEGIN + DELIM + current_mention.tag)
-            elif current_mention.start < i < (current_mention.end - 1):
-                encoded_list.append(INSIDE + DELIM + current_mention.tag)
-            elif i == (current_mention.end - 1):
-                encoded_list.append(INSIDE + DELIM + current_mention.tag)
-                m_num += 1
-            else:
-                encoded_list.append(OUTSIDE)
-        else:
-            encoded_list.append(OUTSIDE)
-    return encoded_list
+# def encode_bio(tokens: list[tuple[str, int]], mentions: list[Mention]) -> list[str]:
+#     """Outputs a list of the BIO tags"""
+#     encoded_list = []
+#     m_num = 0  # The index of the mention in the mentions list
+#     for word, i in tokens:
+#         if m_num < len(mentions):
+#             current_mention = mentions[m_num]
+#             if i == current_mention.start:
+#                 encoded_list.append(BEGIN + DELIM + current_mention.tag)
+#             elif current_mention.start < i < (current_mention.end - 1):
+#                 encoded_list.append(INSIDE + DELIM + current_mention.tag)
+#             elif i == (current_mention.end - 1):
+#                 encoded_list.append(INSIDE + DELIM + current_mention.tag)
+#                 m_num += 1
+#             else:
+#                 encoded_list.append(OUTSIDE)
+#         else:
+#             encoded_list.append(OUTSIDE)
+#     return encoded_list
+
+def encode_bio(tokens: Sequence[str], mentions: Sequence[Mention]) -> list[str]:
+    labels = [OUTSIDE] * len(tokens)
+
+    for entity_type, start, end in mentions:
+        labels[start] = BEGIN + DELIM + entity_type
+        for idx in range(start + 1, end):
+            labels[idx] = INSIDE + DELIM + entity_type
+
+    return labels
 
 
 def create_tok_mention(label: tuple[str, str, str], tok_list: list[tuple[str, int, int]], text: str
@@ -72,47 +82,163 @@ def create_tok_mention(label: tuple[str, str, str], tok_list: list[tuple[str, in
         else:
             i += 1
 
+class Token(NamedTuple):
+    text: str
+    start_char: int
+    end_char: int
+    idx: int
+
+class CharLabel(NamedTuple):
+    label: str
+    start: int
+    end: int
+
+
+class SentenceBuilder:
+    """
+    Mutable class to build up a sentence and it's labels.
+    """
+    def __init__(self, tokens: List[Token]):
+        self.tokens = tokens
+        self.start_char = tokens[0].start_char
+        self.end_char = tokens[-1].end_char
+        self.char_labels: List[CharLabel] = []
+        self.token_labels: List[Mention] = []
+
+    def create_sentence_level_mentions(self):
+        """
+        In place creates token-level labels (Mentions) using char_labels and tokens.
+        """
+        # Build a map from character index to token
+        char_to_token = {char_idx: None for char_idx in range(self.start_char, self.end_char)}
+        for token in self.tokens:
+            for token_char_idx in range(token.start_char, token.end_char):
+                char_to_token[token_char_idx] = token
+        # Lookup the tokens that are in the character offsets, deduplicate with set
+        for char_label in self.char_labels:
+            token_set = set()
+            for char_label_idx in range(char_label.start, char_label.end):
+                # TODO: if you want to handle these differently
+                #   There's a chance docano and stanza have different
+                #    understanding of character offsets
+                if char_label_idx in char_to_token:
+                    token_set.add(char_to_token[char_label_idx])
+                else:
+                    print(f"Warning: Mention {char_label} spans a sentence break")
+                    print([token.text for token in self.tokens])
+                    print("Sent offsets", self.start_char, self.end_char)
+                    print("Continuing with a partial mention labeled\n")
+            # Remove Nones from non token chars like whitespace and sort
+            mention_tokens: List[Token] = sorted([token for token in token_set if token is not None], key=lambda x: x.idx)
+            if mention_tokens:
+                self.token_labels.append(
+                    Mention(char_label.label, mention_tokens[0].idx, mention_tokens[-1].idx + 1)
+                )
+
+    def build(self):
+        """
+        Return immutable version of Mentions for a sentence.
+        """
+        return tuple(self.token_labels)
+
+
+def tokens_from_spacy(tokenized_text) -> List[List[Token]]:
+    sents = []
+    for sent in tokenized_text.sentences:
+        tokens = []
+        for idx, token in enumerate(sent.tokens):
+            tokens.append(
+                Token(
+                    token.text,
+                    token.start_char,
+                    token.end_char,
+                    idx
+                )
+            )
+        sents.append(tokens)
+    return sents
+
+
+def sentence_level_mentions(sentence_tokens: List[List[Token]], labels: tuple[str, str, str]):
+
+    sent_builders = [SentenceBuilder(tokens) for tokens in sentence_tokens]
+    char_idx_to_sent: Dict[int, SentenceBuilder] = {
+        char_idx: sent_builder
+        for sent_builder in sent_builders
+        for char_idx in range(sent_builder.start_char, sent_builder.end_char)
+    }
+
+    for label in labels:
+        start, end, tag = label
+        char_label = CharLabel(tag, int(start), int(end))
+        # Assume the char idx end of the label is in the sentence
+        # If this fails try looking for the start index being in the sentence
+        if char_label.end in char_idx_to_sent:
+            char_idx_to_sent[char_label.end].char_labels.append(char_label)
+        elif char_label.start in char_idx_to_sent:
+            char_idx_to_sent[char_label.start].char_labels.append(char_label)
+        else:
+            print(f"Warning: Couldn't find a sentence for character based label: {CharLabel}\n")
+
+    for sent_builder in sent_builders:
+        sent_builder.create_sentence_level_mentions()
+
+    return [sent_builder.build() for sent_builder in sent_builders]
+
 
 def process_annotations(annotation: dict) -> list[list[tuple[str, str]]]:
     """Changes mention types from character based to token based and
     outputs a list with the token, a space, and the tag"""
-
+    encoded_text = []
     text, labels = annotation["data"], annotation["label"]
     labels.sort(key=lambda a: a[0])
     # text = text.encode("ascii", "replace").decode()
     tokenized_text = nlp(text)
-    encoded_text = []
-    tok_list = []
-    index = 0
-    sentence_list = []
-    # print("*****************************")
-    # determine spans for each token in each sentence
-    for sentence in tokenized_text.sentences:
-        sentence_toks = []
-        for tok in sentence.tokens:
-            token_span = tok.text, tok.start_char, tok.end_char
-            tok_list.append(token_span)
-            sentence_toks.append((token_span[0], index))
-            index += 1
-        sentence_list.append(sentence_toks)
 
-    mention_list = []
-    for label in labels:
-        # for each mention, create a token based mention
-        mention_list.append(create_tok_mention(label, tok_list, tokenized_text.text))
-    for sent_toks in sentence_list:
-        sentence_mentions = []
-        # match the mentions to the sentences
-        for mention in mention_list:
-            if sent_toks[0][1] < mention.start < sent_toks[-1][1]:
-                sentence_mentions.append(mention)
+    sentence_tokens = tokens_from_spacy(tokenized_text)
+    mentions_by_sents = sentence_level_mentions(sentence_tokens, labels)
+
+
+    # encoded_text = []
+    # tok_list = []
+    # index = 0
+    # sentence_list = []
+    # # print("*****************************")
+    # # determine spans for each token in each sentence
+    # for sentence in tokenized_text.sentences:
+    #
+    #     sentence_toks = []
+    #     for tok in sentence.tokens:
+    #         token_span = tok.text, tok.start_char, tok.end_char
+    #         tok_list.append(token_span)
+    #         sentence_toks.append((token_span[0], index))
+    #         index += 1
+    #     sentence_list.append(sentence_toks)
+    #
+    # mention_list = []
+    # for label in labels:
+    #     # for each mention, create a token based mention
+    #     mention_list.append(create_tok_mention(label, tok_list, tokenized_text.text))
+
+
+    # for sent_toks in sentence_list:
+    #
+    #     # TODO:
+    #
+    #     sentence_mentions = []
+    #     # match the mentions to the sentences
+    #     for mention in mention_list:
+    #         if sent_toks[0][1] < mention.start < sent_toks[-1][1]:
+    #             sentence_mentions.append(mention)
+
+    for sent_tokens, sentence_mentions in zip(sentence_tokens, mentions_by_sents):
+        sent_toks = [token.text for token in sent_tokens]
         # bio encode them
-        bio_encoded = encode_bio(sent_toks, sentence_mentions)
-        tokens = [a for a, b in sent_toks]  # get just the token text for the sentence
+        labels = encode_bio(sent_toks, sentence_mentions)
         encoded_sent = []
         # make a list with all of the token-tag pairs for every token
-        for x, y in zip(tokens, bio_encoded):
-            encoded_sent.append((x, y))
+        for token, label in zip(sent_toks, labels):
+            encoded_sent.append((token, label))
         encoded_text.append(encoded_sent)
     return encoded_text
 
