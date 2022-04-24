d4f8546e4631d9ae5066efd5fff72c276779b28f
June Kim <72058960+haewonkim620@users.noreply.github.com>
2022-04-24 19:25:12 -0400

    Update doccano script

--- doccano2conll.py
+++ doccano2conll.py
@@ -1,8 +1,8 @@
 from doccano_transformer.datasets import NERDataset
 from doccano_transformer.utils import read_jsonl
 
-dataset = read_jsonl(filepath='annotated_data/june_week1.jsonl', dataset=NERDataset, encoding='utf-8')
+dataset = read_jsonl(filepath='annotated_data/aparna_week3.jsonl', dataset=NERDataset, encoding='utf-8')
 
-with open("june_conll.txt", "w") as file1:
+with open("aparna_conll.txt", "w") as file1:
     for x in dataset.to_conll2003(tokenizer=str.split):
         print(x['data'], file=file1)
