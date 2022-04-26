647f56ca3d64445cf1406ad45b536f74fbaa3291
Aparna Dutta <aparnad198@gmail.com>
2022-04-26 12:40:59 -0400

    deleted replace_none.py

--- replace_none.py
+++
@@ -1,8 +0,0 @@
-def main():
-    with open("all_annotations.txt", "r") as file1, open("test.txt", "w") as file2:
-        for line in file1:
-            file2.write(line.replace("None", "O"))
-
-
-if __name__ == "__main__":
-    main()
