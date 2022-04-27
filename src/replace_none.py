3e551d3325c7158165dd06fbffc9bf0554573725
June Kim <72058960+haewonkim620@users.noreply.github.com>
2022-04-27 01:12:12 -0400

    Add files for adjudication

---
+++ src/replace_none.py
@@ -0,0 +1,8 @@
+def main():
+    with open("all_annotations.txt", "r") as file1, open("all_annotations_replace_none.txt", "w") as file2:
+        for line in file1:
+            file2.write(line.replace("N/A", "O"))
+
+
+if __name__ == "__main__":
+    main()
