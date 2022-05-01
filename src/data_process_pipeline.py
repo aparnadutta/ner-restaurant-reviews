7e4b3d63b59ffc6ae6cfe3fc421b500343ea0a50
Aparna Dutta <aparnad198@gmail.com>
2022-04-30 21:33:53 -0400

    deleted unused parts of cleaned_reviews and added data_process_pipeline changes

--- src/data_process_pipeline.py
+++ src/data_process_pipeline.py
@@ -19,25 +19,45 @@ def make_conll(annotations, out_path):
     with open(METAFILE, encoding="utf8") as f:  # read in file with metadata
         for line in f:
             metadata = json.loads(line)
+
+    orig_id_to_date = {(md['id'], get_date(md["review_url"])) for md in metadata}
+    sorted_dates = sorted(orig_id_to_date, key=lambda y: y[1])
+    sorted_dates = [sd[0] for sd in sorted_dates]
+
+    # for date in sorted_dates:
+    #     print(date)
+
+    # all_dates = {}
+    # for md in metadata:
+    #     date = get_date(md["review_url"])
+    #     if date in all_dates:
+    #         print(md['id'])
+    #     else:
+    #         all_dates[date] = md['id']
+
     data_with_metadata = defaultdict(lambda: defaultdict(dict, {k: [] for k in (0, 1, 2, "tokens")}))
 
     for annotator_id, annotation in enumerate(annotations):
         for doc in annotation:
             meta_doc = match_meta(doc, metadata)
-            date = meta_doc["date"]
+            doc_id = meta_doc["id"]
+
+            # date = meta_doc["date"]
             for tok_tag_row in meta_doc["data"].split("\n"):
                 if len(tok_tag_row.split()) > 1:   # if the line contains a token and tag
                     tok, _, _, tag = tok_tag_row.split()
-                    data_with_metadata[date]["tokens"].append(tok)
-                    data_with_metadata[date][annotator_id].append(tag)
+                    data_with_metadata[doc_id]["tokens"].append(tok)
+                    data_with_metadata[doc_id][annotator_id].append(tag)
                 else:   # if the line is a DOCSTART or newline
-                    data_with_metadata[date]["tokens"].append(tok_tag_row)
-                    data_with_metadata[date][annotator_id].append("")   # append empty string to keep lists same length
-            if data_with_metadata[date][annotator_id]:
-                data_with_metadata[date]["tokens"] = data_with_metadata[date]["tokens"][: len(data_with_metadata[date][annotator_id])]
+                    data_with_metadata[doc_id]["tokens"].append(tok_tag_row)
+                    data_with_metadata[doc_id][annotator_id].append("")   # append empty string to keep lists same length
+            if data_with_metadata[doc_id][annotator_id]:
+                data_with_metadata[doc_id]["tokens"] = data_with_metadata[doc_id]["tokens"][: len(data_with_metadata[doc_id][annotator_id])]
 
-    data_list = list(data_with_metadata.items())
-    data_list.sort(key=lambda y: y[0])
+    data_list = [(orig_id, data_with_metadata[orig_id]) for orig_id in sorted_dates]
+    # data_list = list(data_with_metadata.items())
+    # data_list.sort(key=lambda y: y[0])
+
     trimmed_data_list = []
     for date, doc_dic in data_list:
         empty_sum = 0
@@ -67,7 +87,7 @@ def make_conll(annotations, out_path):
 def main():
     # read in the properly encoded files
     annotated_files = [read_file(PATH + filename) for filename in FILES]
-    make_conll(annotated_files, "../6_TEST_all_annotations.txt")
+    make_conll(annotated_files, "../UPDATED_all_annotations.txt")
 
 
 if __name__ == "__main__":
